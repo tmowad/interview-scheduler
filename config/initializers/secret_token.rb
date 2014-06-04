5c98c4395e1b2fb987dd8fc4c1706cff66d06382
Tom Mowad <tmowad@gmail.com>
2014-06-03 18:40:00 -0700

    initial raisl project checkin (sans the rails generated README.rdoc since README.md was github generated)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InterviewScheduler::Application.config.secret_key_base = '3e33f52b43671ce05ec8f799955e9ce6b930ea612535beaa66d0feed6d3e6f927715d3d84a55be91c0c7c5fbe191d60393a9951eb109081ad503a8399bd30ac5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InterviewScheduler::Application.config.session_store :cookie_store, key: '_interview-scheduler_session'
